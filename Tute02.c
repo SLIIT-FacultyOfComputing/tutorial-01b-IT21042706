b1858eb3b0dc2b868d02d0ee2f0a9491352c7089
Ridma <87510365+IT21042706@users.noreply.github.com>
2021-07-18 18:36:38 +0000

    All Completed

--- Tute02.c
+++ Tute02.c
@@ -20,5 +20,18 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 int main() {
   
+  int distance, rate;
+  float amount;
+
+  printf("Enter the Distance travelled : ");
+  scanf("%d", &distance);
+
+  if(distance <= 30)
+    amount = distance * 50;
+  else if(distance > 30)
+    amount = 30 * distance + (distance - 30) * 40;
+  
+  printf("Amount to be paid : %.2f", amount);
+  
   return 0;
 }
